fc7695444562a824aa2c2da58da4a95d82d60b5e
MissVy <8860011+MissVy@users.noreply.github.com>
2022-09-23 22:31:21 +1200

    build

---
+++ client/build/precache-manifest.1c16d6200106f121e3bae6218c7f1e4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8166464220a8f18b6de68426a9b0cbc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6e0355535656d91c556",
+    "url": "/static/css/main.7f70dd86.chunk.css"
+  },
+  {
+    "revision": "da782b13275e394ad386",
+    "url": "/static/js/2.61bcc3d7.chunk.js"
+  },
+  {
+    "revision": "652fe30beda1d11f18abc9cfdaebb924",
+    "url": "/static/js/2.61bcc3d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e0355535656d91c556",
+    "url": "/static/js/main.700f29a2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.700f29a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c69638f57ca485ae04",
+    "url": "/static/js/runtime-main.d46f5f9a.js"
+  }
+]);
