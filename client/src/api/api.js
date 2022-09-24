bbe5b1b8d2432f53d91d213f9640aa2db339b65b
MissVy <8860011+MissVy@users.noreply.github.com>
2022-09-24 14:27:11 +1200

    build

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,19 +1,26 @@
-import axios from 'axios';
-const API = axios.create( { baseURL: 'https://plush-baby.herokuapp.com/' });
+import axios from "axios";
+const API = axios.create({ baseURL: "https://plush-baby.herokuapp.com/" });
 
 API.interceptors.request.use((req) => {
-    if(localStorage.getItem('accountProfile')) {
-        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('accountProfile')).token }`;
-    }
-    return req;
-})
+  if (localStorage.getItem("accountProfile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("accountProfile")).token
+    }`;
+  }
+  return req;
+});
 
 export const fetchOneListing = (id) => API.get(`/listings/${id}`);
-export const fetchAllListings = () => API.get('/listings');
-export const createAListing = (newListing) => API.post('/listings', newListing);
-export const updateListing = (id, updatedListing) => API.patch(`/listings/${id}`, updatedListing);
+export const fetchAllListings = () => API.get("/listings");
+export const createAListing = (newListing) => API.post("/listings", newListing);
+export const updateListing = (id, updatedListing) =>
+  API.patch(`/listings/${id}`, updatedListing);
 export const deleteListing = (id) => API.delete(`/listings/${id}`);
-export const commentListing = (value, id) => API.post(`/listings/${id}/commentListing`, { value });
-export const signIn = (signUpsignInFormData) => API.post('/auth/signin', signUpsignInFormData);
-export const signUp = (signUpsignInFormData) => API.post('/auth/signup', signUpsignInFormData);
-export const makeAComment = (id, comment) => API.post(`/listings/${id}`, comment);
+export const commentListing = (value, id) =>
+  API.post(`/listings/${id}/commentListing`, { value });
+export const signIn = (signUpsignInFormData) =>
+  API.post("/auth/signin", signUpsignInFormData);
+export const signUp = (signUpsignInFormData) =>
+  API.post("/auth/signup", signUpsignInFormData);
+export const makeAComment = (id, comment) =>
+  API.post(`/listings/${id}`, comment);
