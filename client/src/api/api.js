ab3ab3b12f2f9b3daf0419e84012e90a4ca44608
MissVy <8860011+MissVy@users.noreply.github.com>
2022-09-24 13:39:00 +1200

    update base url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create( { baseURL: 'http://localhost:3001/' });
+const API = axios.create( { baseURL: 'https://plush-baby.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('accountProfile')) {
